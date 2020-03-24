d60a436b44bda20b99d2d05548fe97bfb34f925e
Jeevan Pillay <jeevan.pillay8@gmail.com>
2020-03-24 20:33:06 +1100

    added initial commit for erc721

---
+++ contracts/ERC721/IERC721.sol
@@ -0,0 +1,11 @@
+// contrats/ERC721.sol
+pragma solidity ^0.6.0;
+
+import "@openzeppelin/contracts/introspection/ERC165.sol";
+
+/**
+ * @dev Required interface of an ERC721 compliant contract.
+ */
+interface IERC721 is IERC165 {
+  
+}
